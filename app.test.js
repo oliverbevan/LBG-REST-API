e6664023ba8fd97bd6bc8b2e3e147139badc667b
oliverbevan <oliver.beave@gmail.com>
2021-11-05 12:49:13 +0000

    added test for UPDATE

--- app.test.js
+++ app.test.js
@@ -27,6 +27,37 @@ describe('CREATE request', () => {
                                 price : 0
                             });
         expect(res.statusCode).toBe(201);
+        expect(res.body.name).toBe("test name")
+        expect(res.body.description).toBe("test desc")
+        expect(res.body.price).toBe(0)
+        // console.log(`res: ${JSON.stringify(res)}`);
+    });
+
+});
+
+describe('UPDATED request', () => {
+    
+    test('CREATE product test and UPDATE', async () => {
+        const res = await request(app)
+                            .post('/product/create')
+                            .send({
+                                name : "banana", 
+                                description : "yellow tasty fruit",
+                                price : 0.20
+                            });
+                            
+        const res2 = await request(app)
+        .put(`/product/update/${JSON.parse(res.text)._id}`)
+        .send({
+            name : "green banana", 
+            description : "green fruit",
+            price : 0.10
+        });
+        
+        expect(res2.statusCode).toBe(202);
+        expect(res2.body.name).toBe("green banana")
+        expect(res2.body.description).toBe("green fruit")
+        expect(res2.body.price).toBe(0.10)
     });
 
 });
